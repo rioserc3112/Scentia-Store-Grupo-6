ce7f395840d741bb474bdb6d2d3942b45cbd9263
rioserc3112 <rioscastro.emmanuel@hotmail.com>
2026-03-06 15:44:38 -0600

    Se crean archivos de ejemplo

---
+++ src/main/java/com/store/controller/storeController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.store.controller;
+
+/**
+ *
+ * @author emma3
+ */
+public class storeController {
+    
+}
